1a22e836043a9d64b07a1eb5c4687ce83ada59ae
avianurag721 <anurag.721721@gmail.com>
2025-02-27 00:31:34 +0530

    Add chjange

--- routes/visitroutes.js
+++ routes/visitroutes.js
@@ -8,6 +8,7 @@ router.put("/update-payment", visitController.updatePaymentStatus);
 router.get("/dashboard", getAdminDashboard);
 router.put("/update-payment/:visitId", visitController.updatePayment);
 router.post("/cancel-visit/:visitId", visitController.cancelVisit);
+router.post("/cancel-test/:visitId/:testId", visitController.cancelTest);
 
 
 module.exports = router;
